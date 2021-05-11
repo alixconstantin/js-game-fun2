498c8831ca6b08fae93f569c420f589888bbf7cc
alixconstantin <64416579+alixconstantin@users.noreply.github.com>
2021-05-11 22:52:28 +0200

    repo

---
+++ game.js
@@ -0,0 +1,28 @@
+let questions = [
+    "How much zodiac have sign ?",
+    "What is the result of 6x12 ?",
+    "What is the year of the capture of the Bastille (French Revolution) ?",
+    "how many legs does a chicken have ?",
+    "how much millions is the french population ?",
+    "what is the year of press freedom in France? ",
+    "in which year in France women obtain the right to vote ?",
+    "What is the result of 2x4 ? ",
+    "What is the result of 234*289.769 ?",
+    "How much foot have humans ?"
+];
+
+let answers = [
+    12,
+    72,
+    1789,
+    2,
+    67,
+    1881,
+    1944,
+    8,
+    67805.946,
+    2
+];
+    
+
+
